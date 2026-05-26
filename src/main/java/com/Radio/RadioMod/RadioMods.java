b62ee1a7db7bc8e7cdf41b5c879d9275ac8d619d
1 <notniy9@gmail.com>
2026-05-26 11:46:57 +0600

    version 1.1

--- src/main/java/com/Radio/RadioMod/RadioMods.java
+++ src/main/java/com/Radio/RadioMod/RadioMods.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.Radio.RadioMod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,8 +7,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = RadioMods.MODID, name = RadioMods.NAME, version = RadioMods.VERSION)
+public class RadioMods
 {
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
